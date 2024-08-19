e1a2eaf25b46dbf3a0129a386127bd8571e59b07
BobiButt <118150902+BobiButt@users.noreply.github.com>
2024-08-19 10:18:54 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-navlinks = document.querySelectorAll('.nav_links');
-lis = document.querySelector('my_list')
-function fun() {
-    // alert("bye");
-    navlinks.className = 'respnsive_nav';
-    lis.className='responsive_nav';
-}
 function msg(){
-    alert("Website is in developement");
+    alert("website is in developement");
+}
+const handleMenu = () => {
+    const nav =  document.getElementById('nav')
+    nav.classList.toggle('active')
+    const ul =  document.getElementById('ul')
+    ul.classList.toggle('active1')
+    // const reg = document.getElementById('reg');
+    // reg.classList.toggle('active1')
 }
